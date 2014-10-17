4e35ee74121f6efa08092bf017044b25899e1b3a
Eduardo Arroyo Ramírez <eduarroyo@mba.local>
2014-10-17 12:11:39 +0200

    Emparejar los resultados obtenidos con el cliente de películas con la tabla de resultados.

---
+++ VideoOpen/app/controllers/fila.js
@@ -0,0 +1,4 @@
+var args = arguments[0] || {};
+
+$.poster.image = args.info.posters.thumbnail;
+$.titulo.text = args.info.title;
--- VideoOpen/app/controllers/peliculas.js
+++ VideoOpen/app/controllers/peliculas.js
@@ -4,13 +4,18 @@ function buscar() {
 	var txtBuscar = $.sbBuscar.value.trim();
 	if(txtBuscar !== '') {
 		var cliente = new (require("filmClient")).filmClient();
+		var data = [];
+		$.tvPeliculas.data = [];
 		cliente.getCine(txtBuscar, function(lista) {
 			if(lista === -1) {
 				alert("Error al cargar las películas");
 			} else {
-				for(var p in lista.movies) {
-					Ti.API.info(lista.movies[p].title);
+				for(var i = 0; i < lista.movies.length; i++) {
+					//Ti.API.info(lista.movies[p].title);
+					data[i] = Alloy.createController('fila', { info: lista.movies[i] }).getView();
 				}
+				
+				$.tvPeliculas.data = data;
 			}
 		});
 	}
