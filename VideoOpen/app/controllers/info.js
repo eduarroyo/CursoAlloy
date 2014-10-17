ca7d9b7bbee7d61e10a5d3e9e83982fba1228e2d
Eduardo Arroyo Ramírez <eduarroyo@mba.local>
2014-10-17 10:48:18 +0200

    Funcionalidad de llamada, email y navegar a web. Ajustes de diseño. Colores de fondo de botones para la depuración.

--- VideoOpen/app/controllers/info.js
+++ VideoOpen/app/controllers/info.js
@@ -1,1 +1,21 @@
 var args = arguments[0] || {};
+
+function llamar(e) {
+	if(OS_IOS) {
+		alert("Llamando por teléfono");
+	} else {
+		Ti.Platform.openURL("tel:555555555");
+	}
+}
+
+function componerEmail(e) {
+	var emailDlg = Titanium.UI.createEmailDialog();
+	emailDlg.subject = "Información sobre el videoclub";
+	emailDlg.toRecipients = ["info@VideoOpen.com"];
+	emailDlg.messageBody = "Escribe tu consulta aquí.";
+	emailDlg.open();
+}
+
+function navegarWeb(e) {
+	Ti.Platform.openURL('http://www.google.es');
+}
